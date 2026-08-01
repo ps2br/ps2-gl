47f39515b78f2c2f7f40d9d110a3e2f9e61c424f
trindadedev13 <trindadedev13@gmail.com>
2026-08-01 03:51:17 +0200

    feat: Add Matrix
    Signed-off-by: trindadedev13 <trindadedev13@gmail.com>

--- include/ps2gl/matrix.h
+++ include/ps2gl/matrix.h
@@ -2,7 +2,27 @@
 #define ps2gl_matrix_h
 
 #include <GL/gl.h>
+#include <linmath.h>
 
-void PS2_MatrixIdentity (GLfloat m[16]);
+#define PS2_MATRIX_STACK_MAX 32
+
+typedef struct ps2_matrix_context
+{
+    mat4x4 ModelView;
+    mat4x4 Projection;
+
+    mat4x4 ModelViewStack[PS2_MATRIX_STACK_MAX];
+    mat4x4 ProjectionStack[PS2_MATRIX_STACK_MAX];
+
+    int ModelViewTop;
+    int ProjectionTop;
+
+    GLenum Mode;
+} PS2_MatrixContext;
+
+mat4x4 *PS2_CurrentMatrix (void);
+void PS2_MatrixIdentity (mat4x4);
+void PS2_MatrixMultiply (mat4x4, const mat4x4, const mat4x4);
+void PS2_TransformVertex (float, float, float, float *, float *, float *);
 
 #endif
--- src/matrix.c
+++ src/matrix.c
@@ -1,18 +1,53 @@
 #include "ps2gl/matrix.h"
 
 #include <GL/gl.h>
+#include <math.h>
 #include <string.h>
+#include <linmath.h>
 
 #include "ps2gl/context.h"
 
 void
-PS2_MatrixIdentity (GLfloat m[16])
+PS2_MatrixIdentity (mat4x4 m)
 {
-    memset (m, 0, sizeof (GLfloat) * 16);
-    m[0] = 1.f;
-    m[5] = 1.f;
-    m[10] = 1.f;
-    m[15] = 1.f;
+    mat4x4_identity(m);
+}
+
+void
+PS2_TransformVertex(float x, float y, float z,
+                    float *outX, float *outY, float *outZ)
+{
+    vec4 v = { x, y, z, 1.0f };
+    vec4 mv;
+    vec4 clip;
+
+    mat4x4_mul_vec4(mv, gl.Matrix.ModelView, v);
+    mat4x4_mul_vec4(clip, gl.Matrix.Projection, mv);
+
+    if (clip[3] != 0.0f)
+    {
+        clip[0] /= clip[3];
+        clip[1] /= clip[3];
+        clip[2] /= clip[3];
+    }
+
+    *outX = clip[0];
+    *outY = clip[1];
+    *outZ = clip[2];
+}
+
+mat4x4 *PS2_CurrentMatrix (void)
+{
+    if (gl.Matrix.Mode == GL_PROJECTION)
+        return &gl.Matrix.Projection;
+
+    return &gl.Matrix.ModelView;
+}
+
+void
+PS2_MatrixMultiply (mat4x4 out, const mat4x4 a, const mat4x4 b)
+{
+    mat4x4_mul(out, a, b);
 }
 
 void
@@ -22,19 +57,154 @@ glMatrixMode (GLenum mode)
     {
     case GL_MODELVIEW:
     case GL_PROJECTION:
-        gl.MatrixMode = mode;
+        gl.Matrix.Mode = mode;
     }
 }
 
 void
 glLoadIdentity (void)
 {
-    if (gl.MatrixMode == GL_MODELVIEW)
+    if (gl.Matrix.Mode == GL_MODELVIEW)
     {
-        PS2_MatrixIdentity (gl.ModelView);
+        PS2_MatrixIdentity (gl.Matrix.ModelView);
     }
-    else if (gl.MatrixMode == GL_PROJECTION)
+    else if (gl.Matrix.Mode == GL_PROJECTION)
     {
-        PS2_MatrixIdentity (gl.Projection);
+        PS2_MatrixIdentity (gl.Matrix.Projection);
     }
+}
+
+void
+glPushMatrix (void)
+{
+
+    if (gl.Matrix.Mode == GL_MODELVIEW)
+    {
+        memcpy (gl.Matrix.ModelViewStack[gl.Matrix.ModelViewTop++],
+                gl.Matrix.ModelView, sizeof (GLfloat) * 16);
+    }
+    else if (gl.Matrix.Mode == GL_PROJECTION)
+    {
+        memcpy (gl.Matrix.ProjectionStack[gl.Matrix.ProjectionTop++],
+                gl.Matrix.Projection, sizeof (GLfloat) * 16);
+    }
+}
+
+void
+glPopMatrix (void)
+{
+    if (gl.Matrix.Mode == GL_MODELVIEW)
+    {
+        memcpy (gl.Matrix.ModelView,
+                gl.Matrix.ModelViewStack[--gl.Matrix.ModelViewTop],
+                sizeof (GLfloat) * 16);
+    }
+    else if (gl.Matrix.Mode == GL_PROJECTION)
+    {
+        memcpy (gl.Matrix.Projection,
+                gl.Matrix.ProjectionStack[--gl.Matrix.ProjectionTop],
+                sizeof (GLfloat) * 16);
+    }
+}
+
+void
+glMultMatrixf (const GLfloat *m)
+{
+    mat4x4 temp;
+    memcpy(temp, m, sizeof(temp));
+
+    mat4x4 *current = PS2_CurrentMatrix();
+
+    mat4x4 result;
+    mat4x4_mul(result, *current, temp);
+
+    memcpy(*current, result, sizeof(result));
+}
+
+void
+glTranslatef (GLfloat x, GLfloat y, GLfloat z)
+{
+    GLfloat m[16] = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, x, y, z, 1 };
+
+    glMultMatrixf (m);
+}
+
+void
+glScalef (GLfloat x, GLfloat y, GLfloat z)
+{
+    GLfloat m[16] = { x, 0, 0, 0, 0, y, 0, 0, 0, 0, z, 0, 0, 0, 0, 1 };
+
+    glMultMatrixf (m);
+}
+
+void
+glRotatef (GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
+{
+    GLfloat rad = angle * 0.0174532925f;
+    GLfloat c = cosf (rad);
+    GLfloat s = sinf (rad);
+
+    GLfloat m[16] = { x * x * (1 - c) + c,
+                      y * x * (1 - c) + z * s,
+                      x * z * (1 - c) - y * s,
+                      0,
+
+                      x * y * (1 - c) - z * s,
+                      y * y * (1 - c) + c,
+                      y * z * (1 - c) + x * s,
+                      0,
+
+                      x * z * (1 - c) + y * s,
+                      y * z * (1 - c) - x * s,
+                      z * z * (1 - c) + c,
+                      0,
+
+                      0,
+                      0,
+                      0,
+                      1 };
+
+    glMultMatrixf (m);
+}
+
+void
+glOrtho (GLdouble left, GLdouble right, GLdouble bottom, GLdouble top,
+         GLdouble znear, GLdouble zfar)
+{
+    GLfloat m[16] = { 0 };
+
+    m[0] = 2.0f / (right - left);
+    m[5] = 2.0f / (top - bottom);
+    m[10] = -2.0f / (zfar - znear);
+
+    m[12] = -(right + left) / (right - left);
+    m[13] = -(top + bottom) / (top - bottom);
+    m[14] = -(zfar + znear) / (zfar - znear);
+
+    m[15] = 1.0f;
+
+    glMultMatrixf (m);
+}
+
+void
+glFrustum (GLdouble left, GLdouble right, GLdouble bottom, GLdouble top,
+           GLdouble znear, GLdouble zfar)
+{
+    GLfloat m[16] = { 0 };
+
+    m[0] = (2.0f * znear) / (right - left);
+
+    m[5] = (2.0f * znear) / (top - bottom);
+
+    m[8] = (right + left) / (right - left);
+
+    m[9] = (top + bottom) / (top - bottom);
+
+    m[10] = -(zfar + znear) / (zfar - znear);
+
+    m[11] = -1.0f;
+
+    m[14] = -(2.0f * zfar * znear) / (zfar - znear);
+
+    glMultMatrixf (m);
 }
